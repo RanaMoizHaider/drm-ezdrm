9fc31380641bbae60ba18beff014eed8dafe2f3e
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-12 00:44:32 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [VideoController::class, 'index'])->name('video.index');
 Route::get('/upload', [VideoController::class, 'showUploadForm'])->name('video.upload.form');
 Route::post('/upload', [VideoController::class, 'uploadVideo'])->name('video.upload');
-Route::get('/play/{videoID}', [VideoController::class, 'playVideo'])->name('video.play');
+Route::get('/play/{token}', [VideoController::class, 'playVideo'])->name('video.play');
