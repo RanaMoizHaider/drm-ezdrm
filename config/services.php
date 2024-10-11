e8e556d1a404e1106373698128030c8aab9172a3
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-12 00:44:44 +0500

    Update services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,29 @@ return [
         ],
     ],
 
+    'aws' => [
+        'endpoint' => env('AWS_MEDIACONVERT_ENDPOINT'),
+        'role' => env('AWS_MEDIACONVERT_ROLE'),
+        'region' => env('AWS_MEDIACONVERT_REGION'),
+        'key' => env('AWS_MEDIACONVERT_ACCESS_KEY_ID'),
+        'secret' => env('AWS_MEDIACONVERT_SECRET_ACCESS_KEY'),
+        'speke' => env('SPEKE_SERVER_URL'),
+        'queue' => env('AWS_MEDIACONVERT_QUEUE'),
+    ],
+
+    'ezdrm' => [
+        'key' => env('EZDRM_KEY'),
+        'username' => env('EZDRM_USERNAME'),
+        'password' => env('EZDRM_PASSWORD'),
+    ],
+
+    'resource_id' => env('RESOURCE_ID'),
+
+    'system_ids' => [
+        'widevine' => env('WIDEVINE_ID'),
+        'marlin' => env('MARLIN_ID'),
+        'playready' => env('PLAYREADY_ID'),
+        'fairplay' => env('FAIRPLAY_ID'),
+    ],
+
 ];
