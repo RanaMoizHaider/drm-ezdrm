7e090484d93f1b6defeacb82792250b1d349a3ef
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-12 20:08:48 +0500

    removed video and audio robustness updation from frontend

--- resources/views/video/play.blade.php
+++ resources/views/video/play.blade.php
@@ -17,30 +17,6 @@
 
             <span id="browserCheckResult"></span>
 
-            <!-- Dropdown for video robustness levels -->
-            <div class="mt-4">
-                <h4>Select Video Robustness:</h4>
-                <select id="videoR" onchange="updateSettings()">
-                    <option value="SW_SECURE_CRYPTO">SW_SECURE_CRYPTO</option>
-                    <option value="SW_SECURE_DECODE">SW_SECURE_DECODE</option>
-                    <option value="HW_SECURE_CRYPTO">HW_SECURE_CRYPTO</option>
-                    <option value="HW_SECURE_DECODE">HW_SECURE_DECODE</option>
-                    <option value="HW_SECURE_ALL" selected>HW_SECURE_ALL</option>
-                </select>
-            </div>
-
-            <!-- Dropdown for audio robustness levels -->
-            <div class="mt-4">
-                <h4>Select Audio Robustness:</h4>
-                <select id="audioR" onchange="updateSettings()">
-                    <option value="SW_SECURE_CRYPTO">SW_SECURE_CRYPTO</option>
-                    <option value="SW_SECURE_DECODE">SW_SECURE_DECODE</option>
-                    <option value="HW_SECURE_CRYPTO" selected>HW_SECURE_CRYPTO</option>
-                    <option value="HW_SECURE_DECODE">HW_SECURE_DECODE</option>
-                    <option value="HW_SECURE_ALL">HW_SECURE_ALL</option>
-                </select>
-            </div>
-
             <div style="min-height:120px;">
                 <span id="log"></span>
             </div>
@@ -60,14 +36,6 @@
 
             let videoR = 'HW_SECURE_ALL';
             let audioR = 'HW_SECURE_CRYPTO';
-
-            function updateSettings() {
-                videoR = document.getElementById('videoR').value;
-                audioR = document.getElementById('audioR').value;
-
-                // Reinitialize the player
-                initApp();
-            }
         </script>
         <script src="{{ asset('js/helper.js') }}"></script>
         <script src="{{ asset('js/shaka.js') }}"></script>
