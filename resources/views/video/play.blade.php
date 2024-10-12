e65661254b29baca547e3bdd708cf537c41d118a
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-12 10:28:26 +0500

    changes video js to shaka player

--- resources/views/video/play.blade.php
+++ resources/views/video/play.blade.php
@@ -1,8 +1,8 @@
 {{--
     File: play.blade.php
-    Description: View to play a video using VdoCipher player.
+    Description: View to play a video using Shaka Player.
     Author: Moiz Haider
-    Date: 11 October 2024
+    Date: 12 October 2024
 --}}
 
 <x-layouts.app>
@@ -10,59 +10,84 @@
     <x-slot:title>Play Video</x-slot:title>
 
     <x-slot:styles>
-        <!-- Video.js CSS -->
-        <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet" />
+        <!-- Shaka Player UI CSS -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/shaka-player/4.11.7/controls.min.css">
     </x-slot:styles>
 
     <div class="flex justify-center items-center">
-        <div class="relative w-full md:w-2/3 lg:w-1/2">
-            <!-- Video.js player -->
-            <video id="video-player" class="video-js vjs-default-skin vjs-16-9" controls preload="auto" width="640" height="360">
-                <!-- Placeholder for poster image -->
-                <p class="vjs-no-js">
-                    To view this video please enable JavaScript, and consider upgrading to a
-                    web browser that
-                    <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-                </p>
-            </video>
+        <div id="video-container" class="relative w-full md:w-2/3 lg:w-1/2">
+            <!-- Shaka Player video element -->
+            <video id="video-player" width="640" controls></video>
         </div>
     </div>
 
     <x-slot:scripts>
-        <!-- Video.js and DASH.js -->
-        <script src="https://unpkg.com/video.js/dist/video.js"></script>
-        <!-- Include Dash.js library -->
-        <script src="https://cdn.dashjs.org/latest/dash.all.min.js"></script>
-        <!-- Include videojs-contrib-dash -->
-        <script src="https://unpkg.com/videojs-contrib-dash/dist/videojs-dash.js"></script>
-        <!-- Include videojs-contrib-eme -->
-        <script src="https://unpkg.com/videojs-contrib-eme/dist/videojs-contrib-eme.js"></script>
+        <!-- Shaka Player UI JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/shaka-player/4.11.7/shaka-player.ui.min.js"></script>
         <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var player = videojs('video-player');
-                player.eme();
-                player.eme.detectSupportedCDMs()
-                    .then(supportedCDMs => {
-                        console.log(supportedCDMs);
-                    });
-                player.ready(function () {
-                    player.src({
-                        src: '{{ $videoUrl }}',
-                        type: 'application/dash+xml',
-                        keySystems: {
-                            'com.widevine.alpha': {
-                                licenseUri: 'https://widevine-dash.ezdrm.com/proxy?pX=D6A082',
-                                audioRobustness: 'HW_SECURE_DECODE',
-                                videoRobustness: 'HW_SECURE_DECODE'
-                            },
-                            'com.microsoft.playready': {
-                                licenseUri: 'https://playready.ezdrm.com/cency/preauth.aspx?pX=2AFB63'
+            document.addEventListener('DOMContentLoaded', initApp);
 
+            function initApp() {
+                // Install built-in polyfills to patch browser incompatibilities.
+                shaka.polyfill.installAll();
+
+                // Check if the browser supports the basic APIs Shaka needs.
+                if (shaka.Player.isBrowserSupported()) {
+                    // Everything looks good!
+                    initPlayer();
+                } else {
+                    // This browser does not have the minimum set of APIs we need.
+                    console.error('Browser not supported!');
+                }
+            }
+
+            function initPlayer() {
+                // Create a Player instance.
+                var video = document.getElementById('video-player');
+                var videoContainer = document.getElementById('video-container');
+
+                var player = new shaka.Player(video);
+                var ui = new shaka.ui.Overlay(player, videoContainer, video);
+
+                // Attach player and UI to the window for easy access in the JS console.
+                window.player = player;
+                window.ui = ui;
+
+                // Listen for error events.
+                player.addEventListener('error', onErrorEvent);
+
+                // Configure DRM servers and robustness.
+                player.configure({
+                    drm: {
+                        servers: {
+                            'com.widevine.alpha': 'https://widevine-dash.ezdrm.com/proxy?pX=D6A082',
+                            'com.microsoft.playready': 'https://playready.ezdrm.com/cency/preauth.aspx?pX=2AFB63'
+                        },
+                        advanced: {
+                            'com.widevine.alpha': {
+                                'videoRobustness': 'HW_SECURE_DECODE',
+                                'audioRobustness': 'HW_SECURE_DECODE'
                             }
                         }
-                    });
+                    }
                 });
-            });
+
+                // Load the manifest URI.
+                player.load('{{ $videoUrl }}').then(function() {
+                    // The video has now been loaded.
+                    console.log('The video has now been loaded!');
+                }).catch(onError);  // Handle asynchronous load errors.
+            }
+
+            function onErrorEvent(event) {
+                // Extract the shaka.util.Error object from the event.
+                onError(event.detail);
+            }
+
+            function onError(error) {
+                // Log the error.
+                console.error('Error code', error.code, 'object', error);
+            }
         </script>
     </x-slot:scripts>
 
